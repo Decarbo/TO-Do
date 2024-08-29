2f332aae3ede7620c027d5e6021eb79ee4e495b6
Decarbo <decarbo77@gmail.com>
2024-08-29 16:07:08 +0530

    newwwww

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-const inputbox = document.getElementById('inputbox')
-const listcontainer = document.getElementById('listcontainer')
-function addtask(){
-    if(inputbox.value===''){
-        alert("Bro First Add your Task")
-    }
-    else{
+const inputbox = document.getElementById("inputbox");
+const listcontainer = document.getElementById("listcontainer");
+function addtask() {
+    if (inputbox.value === "") {
+        alert("Bro First Add your Task");
+    } else {
         let li = document.createElement("li");
         li.innerHTML = inputbox.value;
         listcontainer.appendChild(li);
@@ -12,25 +11,28 @@ function addtask(){
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
-    inputbox.value= "";
+    inputbox.value = "";
     save();
 }
 
-listcontainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("check");
-        save();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        save();
-    }
-},false);
+listcontainer.addEventListener(
+    "click",
+    function (e) {
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("check");
+            save();
+        } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+            save();
+        }
+    },
+    false
+);
 
-function save(){
+function save() {
     localStorage.setItem("data", listcontainer.innerHTML);
 }
-function show(){
+function show() {
     listcontainer.innerHTML = localStorage.getItem("data");
 }
 show();
